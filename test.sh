755c450d928fd64be988ba196088a3116ce947b6
sunsheng <287886999@qq.com>
2022-10-01 21:05:36 +0800

    配置反向代理

--- test.sh
+++ test.sh
@@ -2,11 +2,12 @@
 
 # set -x
 adb shell kill $(adb shell pidof lighttpd)
-set -e
+# set -e
 adb shell "mkdir -p /data/local/tmp/conf"
 adb shell "mkdir -p /data/local/tmp/www"
 adb shell "mkdir -p /data/local/tmp/logs"
 adb shell "mkdir -p /data/local/tmp/run"
+adb shell "mkdir -p /data/local/tmp/upload"
 adb shell "mkdir -p /data/local/tmp/cache/compress"
 adb shell "echo \"lighttpd test $(date)\" > /data/local/tmp/www/index.html"
 
@@ -14,4 +15,8 @@ adb push lighttpd.conf /data/local/tmp/conf
 adb push sbin/lighttpd /system/bin
 adb shell /system/bin/lighttpd -f /data/local/tmp/conf/lighttpd.conf
 adb forward tcp:8000 tcp:80
-curl -H "Accept-Encoding: gzip" http://127.0.0.1:8000/index.html -v
+
+# curl -v http://127.0.0.1:8000/
+
+curl -v http://127.0.0.1:8000/lighttpd-arm/
+
